a08482cf202389bdcf1521412097a26fc49f9a6e
wladimir <wladimir12032005@gmail.com>
2025-03-19 18:53:04 +0700

    Init frontend

---
+++ frontend/zubrilaFrontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+import RegistrationComponent from "./components/registrationComponent.vue";
+</script>
+
+<template>
+  <registration-component></registration-component>
+</template>
+
---
+++ frontend/zubrilaFrontend/src/components/registrationComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="container">
+      <input>
+      <input>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
